3444e099b4ea1284b45f87bad0d7cd1f4289c75a
yozintt <yoanderfl@gmail.com>
2021-12-09 08:41:15 -0300

    pronto

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,5 +1,18 @@
 <?php
 
+$mensagem = '';
+ if(isset($_GET['status'])){
+   switch ($_GET['status']) {
+     case 'success':
+       $mensagem = '<div class="alert alert-success">Ação executada com sucesso!</div>';
+       break;
+
+     case 'error':
+       $mensagem = '<div class="alert alert-danger">Ação não executada!</div>';
+       break;
+   }
+ }
+
 $resultados = "";
 foreach($pessoas as $pessoa){
     $resultados .= '<tr>
