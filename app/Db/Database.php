34b092a9ad96f2d63e174d4e154a63fd9add88d7
yozintt <yoanderfl@gmail.com>
2021-12-09 08:53:34 -0300

    pronto

--- app/Db/Database.php
+++ app/Db/Database.php
@@ -40,30 +40,46 @@ class Database{
             $statement = $this->connection->prepare($query);
             $statement->execute($params);
             return $statement;
-
         }catch(PDOException $e){
             die('ERROR: '.$e->getMessage) )
     }
-
+}
     public function insert($values){
        
         $fields = array_keys($values);
         $bids = array_pad([], count($fields), ' ?');
 
         $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+        
         $this->execute($query.array_values($values));
 
         return $this->connection->lastInserteId();
     }
 
-    public function select($where = null, $order = null, $limit = null, $fields = '*'){
-       
-       $where = strlen($where) ? 'WHERE '.$where : '';
-       $order = strlen($order) ? 'ORDER BY '.$order : '';
-       $limit = strlen($limit) ? 'LIMIT '.$limit : '';  
-       
-        $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order. ' '.$limit;
+    public function update($where,$values){
         
-        return $this->execute($query);
+        $fields = array_keys($values);
+    
+       
+        $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+    
+       
+        $this->execute($query,array_values($values));
+    
+        
+        return true;
+      }
+    
+    
+      public function delete($where){
+       
+        $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+    
+       
+        $this->execute($query);
+    
+       
+        return true;
+      }
+    
     }
-}
